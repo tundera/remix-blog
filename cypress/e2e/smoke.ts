e17c2dddffdf003e43bb2f226306b64f616df599
Philip Johnston <tunderadev@gmail.com>
2022-03-27 20:59:15 -0400

    add cypress testing

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -1,48 +1,15 @@
-import faker from "@faker-js/faker";
-
 describe("smoke tests", () => {
-  afterEach(() => {
-    cy.cleanupUser();
-  });
+  afterEach(() => {});
 
-  it("should allow you to register and login", () => {
-    const loginForm = {
-      email: `${faker.internet.userName()}@example.com`,
-      password: faker.internet.password(),
-    };
-    cy.then(() => ({ email: loginForm.email })).as("user");
-
+  it("should allow you view posts", () => {
     cy.visit("/");
-    cy.findByRole("link", { name: /sign up/i }).click();
+    cy.findByRole("link", { name: /blog posts/i }).click();
 
-    cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
-    cy.findByLabelText(/password/i).type(loginForm.password);
-    cy.findByRole("button", { name: /create account/i }).click();
+    cy.findByRole("link", { name: /90s mixtape/i }).click();
 
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByRole("button", { name: /logout/i }).click();
-    cy.findByRole("link", { name: /log in/i });
-  });
-
-  it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
-    };
-    cy.login();
-    cy.visit("/");
-
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByText("No notes yet");
-
-    cy.findByRole("link", { name: /\+ new note/i }).click();
-
-    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
-    cy.findByRole("button", { name: /save/i }).click();
-
-    cy.findByRole("button", { name: /delete/i }).click();
-
-    cy.findByText("No notes yet");
+    cy.findByText("I wish (Skee-Lo)");
+    cy.findByText("This Is How We Do It (Montell Jordan)");
+    cy.findByText("Everlong (Foo Fighters)");
+    cy.findByText("Ms. Jackson (Outkast)");
   });
 });
