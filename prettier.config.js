45864b22a249f334d36fa943d85c8605d1c79681
Philip Johnston <tunderadev@gmail.com>
2022-03-29 08:59:56 -0400

    style(git): 🔧  setup git hooks
    Add git hooks for commit message linting and code linting/formatting.

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  proseWrap: 'always',
+}
